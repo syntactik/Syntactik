af397837a611bdd98e85f3963d1f38e242459ef7
Maxim Trushin <trushin@gmail.com>
2017-11-07 11:25:06 -0500

    Added SharedAssemblyInfo.

--- slc/Properties/AssemblyInfo.cs
+++ slc/Properties/AssemblyInfo.cs
@@ -4,30 +4,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Syntactik")]
-[assembly: AssemblyDescription("Syntactik. Proprocessor for XML and JSON.")]
+[assembly: AssemblyTitle("Syntactik command tool.")]
+[assembly: AssemblyDescription("Syntactik. Preprocessor for XML and JSON.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Syntactik llc")]
-[assembly: AssemblyProduct("Syntactik")]
-[assembly: AssemblyCopyright("Copyright © Maxim Trushin, Syntactik llc,  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("14FF4AEA-B274-4DF7-8E3E-AC46395DE0E3")] 
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("0.0.1.*")]
