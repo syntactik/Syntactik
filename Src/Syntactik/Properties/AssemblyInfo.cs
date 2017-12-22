3b1233443b3e88897f7664c4192b702f18eb5868
Maxim Trushin <trushin@gmail.com>
2017-12-22 17:26:44 -0500

    Added more info to project file.

--- Src/Syntactik/Properties/AssemblyInfo.cs
+++ Src/Syntactik/Properties/AssemblyInfo.cs
@@ -21,8 +21,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyDescription("Syntactik. Preprocessor for XML and JSON.")]
-
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b36ec10c-824d-4023-a32e-8bc9369869ae")]
